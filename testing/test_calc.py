90ca50e88cbfe0f2c26406be7b01fac0262c16b8
shirleyxu <shirleyxu0506@163.com>
2020-05-31 16:19:35 +0800

    python pytest 实战2(自动添加标签，只运行标签add,div，封装步骤到yaml待完善)

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -10,24 +10,30 @@ class TestCalc:
     def setup(self):
         self.calc = Calc()
 
+    @pytest.mark.run(order=2)
+    # @pytest.mark.add
     @pytest.mark.parametrize('a, b, expected', yaml.safe_load(open("../data/calc_add.yaml")))
-    def test_add(self, a, b, expected):
+    def calc_add(self, a, b, expected):
         try:
             actual_result = self.calc.add(a, b)
             assert round(actual_result, 2) == expected
         except TypeError:
             print("请输入数字")
 
+    @pytest.mark.run(order=1)
+    # @pytest.mark.sub
     @pytest.mark.parametrize('a, b, expected', yaml.safe_load(open("../data/calc_sub.yaml")))
-    def test_sub(self, a, b, expected):
+    def calc_sub(self, a, b, expected):
         try:
             actual_result = self.calc.sub(a, b)
             assert round(actual_result, 2) == expected
         except TypeError:
             print("请输入数字")
 
+    @pytest.mark.run(order=3)
+    # @pytest.mark.div
     @pytest.mark.parametrize('a, b, expected', yaml.safe_load(open("../data/calc_div.yaml")))
-    def test_div(self, a, b, expected):
+    def calc_div(self, a, b, expected):
         try:
             if b == 0:
                 print("除数不能为0")
@@ -37,8 +43,10 @@ class TestCalc:
         except TypeError:
             print("请输入数字")
 
+    @pytest.mark.run(order=4)
+    # @pytest.mark.div
     @pytest.mark.parametrize('a, b, expected', yaml.safe_load(open("../data/calc_mul.yaml")))
-    def test_mul(self, a, b, expected):
+    def calc_div(self, a, b, expected):
         try:
             actual_result = self.calc.mul(a, b)
             assert round(actual_result, 2) == expected
@@ -46,8 +54,5 @@ class TestCalc:
             print("请输入数字")
 
 
-
-
-
-
-
+# if __name__ == '__main__':
+    # pytest.main(["-m", "test_calc.py::TestCalc::div"])
