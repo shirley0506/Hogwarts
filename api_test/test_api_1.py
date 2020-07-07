6d393239a8fbc6528037d222b6c2d861f250e1a4
shirleyxu <shirleyxu0506@163.com>
2020-07-07 16:59:48 +0800

    接口测试实战2课间作业

--- api_test/test_api_1.py
+++ api_test/test_api_1.py
@@ -1,50 +1,102 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import pytest
 import requests
 
+
 # 获取token
+from filelock import FileLock
+
+
+@pytest.fixture(scope='session')
 def test_token():
-    corpid = 'wwb9dd59b36dfb3ab0'
-    corpsecret = 'Bg-l59yCtimW-H8rBSiRC_IWDsF07awtF0UGTtb3KuU'
-    req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}")
+    with FileLock("session.lock"):
+        corpid = 'wwb9dd59b36dfb3ab0'
+        corpsecret = 'Bg-l59yCtimW-H8rBSiRC_IWDsF07awtF0UGTtb3KuU'
+        req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}")
     return req.json()['access_token']
 
 
 # 创建部门
-def test_create_dep():
+# @pytest.mark.parametrize("name, name_en, ID", [('上海研发中心2', 'Develop2', 2)])
+def test_create_dep(name, name_en, ID, test_token):
     data = {
-        "name": "测试",
-        "name_en": "Testing",
-        "parentid": 1
+        "name": name,
+        "name_en": name_en,
+        "parentid": 1,
+        "id": ID
     }
-
-    req = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/department/create?access_token={test_token()}",
+    req = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/department/create?access_token={test_token}",
                         json=data
                         )
+    return req.json()
 
 
 # 修改部门信息
-def test_edit_dep():
+def test_edit_dep(ID, name, test_token):
     data = {
-        "id": 2,
-        "name": "产品测试",
+        "id": ID,
+        "name": name,
     }
-    req = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/department/update?access_token={test_token()}",
+    req = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/department/update?access_token={test_token}",
                         json=data
                         )
-    print(req.json())
+    return req.json()
 
 
 # 获取部门列表
-def test_get_dep():
-    req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token={test_token()}")
-    print(req.json())
+def test_get_dep(ID, test_token):
+    req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token={test_token}&id={ID}")
+    # print(req.json())
+    return req.json()
 
 
 # 删除部门
-def test_del_dep():
-    dep_id = 2
-    req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/delete?access_token={test_token()}&id={dep_id}")
-    print(req.json())
+def test_del_dep(ID, test_token):
+    req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/delete?access_token={test_token}&id={ID}")
+    # print(req.json())
+    return req.json()
+
+
+# 根据部门名称获取部门ID
+# @pytest.mark.parametrize("name", ['上海研发中心'])
+def test_get_DepID(name, test_token):
+    get_all_deps = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token={test_token}")
+    list_deps = get_all_deps.json()['department']
+    for dep in list_deps:
+        if name == dep['name']:
+            del_id = dep['id']
+            return del_id
+
+
+# 准备测试数据
+def test_create_data():
+    data = [("上海研发中心" + str(i),
+             "Develop" + str(i),
+             i) for i in range(3, 10)]
+    return data
+
+
+# 测试用例
+@pytest.mark.parametrize("name, name_en, ID", test_create_data())
+def test_all(name, name_en, ID, test_token):
+    try:
+        assert "created" == test_create_dep(name, name_en, ID, test_token)['errmsg']
+    except AssertionError as e:
+        # 如果部门名称重复或者部门ID重复，找到同名的部门id并删除
+        if "department existed" in e.__str__():
+            del_id = test_get_DepID(name, test_token)
+            if del_id != 'None':
+                assert "deleted" == test_del_dep(del_id, test_token)['errmsg']
+                assert "created" == test_create_dep(name, name_en, ID, test_token)['errmsg']
+            else:
+                assert "deleted" == test_del_dep(ID, test_token)['errmsg']
+                assert "created" == test_create_dep(name, name_en, ID, test_token)['errmsg']
+    assert name == test_get_dep(ID, test_token)['department'][0]['name']
+    assert "updated" == test_edit_dep(ID, '北京研发中心', test_token)['errmsg']
+    assert "北京研发中心" == test_get_dep(ID, test_token)['department'][0]['name']
+    assert "deleted" == test_del_dep(ID, test_token)['errmsg']
+    assert 60123 == test_del_dep(ID, test_token)['errcode']
+
+
 
--- api_test/test_api_2.py
+++ api_test/test_api_2.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+# -*- coding: UTF8 -*-
+import random
 import re
 
 import pytest
 import requests
+from filelock import FileLock
 
 
 @pytest.fixture(scope='session')
 def test_token():
-    corpid = 'wwb9dd59b36dfb3ab0'
-    corpsecret = 'Bg-l59yCtimW-H8rBSiRC_IWDsF07awtF0UGTtb3KuU'
-    req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}")
-    print("test_token")
+    with FileLock("session.lock"):
+        corpid = 'wwb9dd59b36dfb3ab0'
+        corpsecret = 'Bg-l59yCtimW-H8rBSiRC_IWDsF07awtF0UGTtb3KuU'
+        req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}")
+    # print("test_token")
     return req.json()['access_token']
 
 
-def test_create(userid, name, mobile):
+def test_create(userid, name, mobile, test_token):
     data = {
         "userid": userid,
         "name": name,
         "mobile": mobile,
-        "department": 1
+        "department": [1]
     }
     req = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={test_token}",
                         json=data
@@ -29,24 +32,58 @@ def test_create(userid, name, mobile):
     # print(req.json())
 
 
-def test_get():
-    userid = "zhangsan"
+def test_get(userid, test_token):
+    # userid = "zhangsan"
     req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={test_token}&userid={userid}")
     # print(req.json())
-
-
-def test_delete(userid):
-    # userid = "zhangsan"
-    req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={test_token}&userid={userid}")
     return req.json()
 
 
-@pytest.mark.parametrize("userid, name, mobile", [("zhangsan1", "张三1", "12345678902")])
-def test_all(userid, name, mobile):
+def test_update(userid, name, mobile, test_token):
+    data = {
+        "userid": userid,
+        "mobile": mobile,
+        "name": name
+    }
+    req = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={test_token}",
+                        json=data
+                        )
+    return req.json()
+
+
+def test_delete(userid, test_token):
+    # userid = "zhangsan"
+    req = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={test_token}&userid={userid}")
+    # print(req.json())
+    return req.json()
+
+
+def test_creat_data():
+    data = [("zhangsan" + str(x),
+             "张三",
+             "138%08d" % x) for x in range(10)]
+    # print(data)
+    return data
+
+
+@pytest.mark.parametrize("userid, name, mobile", test_creat_data())
+def test_all(userid, name, mobile, test_token):
     try:
-        assert "created" == test_create(userid, name, mobile)['errmsg']
+        assert "created" == test_create(userid, name, mobile, test_token)['errmsg']
     except AssertionError as e:
+        # 如果用户手机号被使用，找到手机号的userid，并删除
         if "mobile existed" in e.__str__():
-            re_userid = re.findall(":(.*)'$", e.__str__())[0]
-            assert "deleted" == test_delete(userid)['errmsg']
+            re_userid = re.findall(":(.*)$", e.__str__())[0]
+            if re_userid.endswith("'") or re_userid.endswith('"'):
+                re_userid = re_userid[:-1]
+            assert "deleted" == test_delete(re_userid, test_token)['errmsg']
+            assert 60111 == test_get(re_userid, test_token)['errcode']
+            assert "created" == test_create(userid, name, mobile, test_token)['errmsg']
+    assert name == test_get(userid, test_token)['name']
+    assert "updated" == test_update(userid, "测试", mobile, test_token)['errmsg']
+    assert "测试" == test_get(userid, test_token)['name']
+    assert "deleted" == test_delete(userid, test_token)['errmsg']
+    assert 60111 == test_get(userid, test_token)['errcode']
+
+
 
